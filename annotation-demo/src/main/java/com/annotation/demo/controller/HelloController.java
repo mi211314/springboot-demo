13fd2abf537a3439afc89e6bb2310b96706a2808
miaolei <miaolei@niwodai.net>
2018-08-02 16:55:48 +0800

    annotation-demo项目提交。

---
+++ annotation-demo/src/main/java/com/annotation/demo/controller/HelloController.java
@@ -0,0 +1,20 @@
+package com.annotation.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * @Description: HelloController
+ * @Author: miaolei
+ * @Date: 2018/8/2 16:38
+ */
+@Controller
+public class HelloController {
+
+    @RequestMapping(value="/hello", method=RequestMethod.GET)
+    public String sayHello() {
+        return "hello";
+    }
+
+}
