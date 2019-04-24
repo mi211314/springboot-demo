3f9e79be04affac20038b1d9f45fc53c767b359c
mi211314 <447183513@qq.com>
2019-04-25 07:42:44 +0800

    springboot-demo1项目代码提交。

---
+++ finance-market-deploy/finance-market-web/src/main/java/com/demo/financemarket/FinanceMarketWebApplication.java
@@ -0,0 +1,13 @@
+package com.demo.financemarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceMarketWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinanceMarketWebApplication.class, args);
+    }
+
+}
---
+++ finance-market-erp-deploy/finance-market-erp-web/src/main/java/com/demo/financemarket/erp/FinanceMarketErpWebApplication.java
@@ -0,0 +1,13 @@
+package com.demo.financemarket.erp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceMarketErpWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinanceMarketErpWebApplication.class, args);
+    }
+
+}
